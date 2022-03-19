094d26895d4998894eb784f95012ebb7024938ea
dragynir <d.korostelev@g.nsu.ru>
2022-03-19 18:17:12 +0700

    exp0

--- src/data/transformations.py
+++ src/data/transformations.py
@@ -8,8 +8,9 @@ data_transforms = {
         A.Resize(CONFIG['img_height'], CONFIG['img_width']),
         A.ShiftScaleRotate(shift_limit=0.1,
                            scale_limit=0.15,
-                           rotate_limit=60,
+                           rotate_limit=10,
                            p=0.5),
+        A.HorizontalFlip(p=0.5),
         A.HueSaturationValue(
             hue_shift_limit=0.2,
             sat_shift_limit=0.2,
